e48de728a4a3698ff9e526f969af84514af38b84
Vinicius Alexandre <viniciusalexandrerodrigues7@gmail.com>
2025-06-15 01:06:18 -0300

    Implementação inicial de um CRUD com POO utilizando a API do google books

--- Gerenciador_Livros.py
+++ Gerenciador_Livros.py
@@ -1,5 +1,7 @@
 import os
 import json
+import requests
+import urllib.parse
 
 class Livro:
 
@@ -11,7 +13,7 @@ class Livro:
         self.status = status
 
     def __str__(self):
-        return f"'{self.titulo}' por {self.autor} ({self.ano_publicado}) - Status: {self.status}"
+        return f"{self.titulo} por {self.autor} ({self.ano_publicado}) - Status: {self.status}"
 
 
 class Biblioteca:
@@ -20,8 +22,7 @@ class Biblioteca:
 
         self.livros = []
         self.nome_arquivo = nome_arquivo
-        #self.carregar_de_json()
-        pass
+        self.carregar_de_json()
 
     def adicionar_livro(self, livro_obj):
 
@@ -35,6 +36,40 @@ class Biblioteca:
     def listar_livros(self):
         return self.livros
 
+
+    def salvar_em_json(self):
+
+        salvamento_dos_dados = [{ 
+        "titulo":livro.titulo,
+        "autor":livro.autor,
+        "ano_publicado":livro.ano_publicado,
+        "status":livro.status}
+        
+        for livro in self.livros
+
+        ]
+
+        with open(self.nome_arquivo, "w", encoding="utf-8") as arquivo_json:
+            json.dump(salvamento_dos_dados, arquivo_json, indent=4)
+
+        print(f"Biblioteca salva em {self.nome_arquivo} com sucesso")
+
+
+    def carregar_de_json(self):
+
+        if os.path.exists(self.nome_arquivo):
+
+            try:
+                with open(self.nome_arquivo, "r", encoding="utf-8") as arquivo_json:
+                    dados_lido_do_arquivo = json.load(arquivo_json)
+
+                    for dados in dados_lido_do_arquivo:
+                        novo_livro = Livro(dados["titulo"], dados["autor"], dados["ano_publicado"], dados["status"])
+                        self.livros.append(novo_livro)
+
+            except json.JSONDecodeError:
+                pass
+
     
     #================
     #   Interface
@@ -45,17 +80,85 @@ def gerenciar_adicao(biblioteca_obj):
 
     print("\n--- Adicionar Novo Livro ---")
 
-    titulo = input("Digite o título do livro: ")
-    autor = input("Digite o nome do autor do livro: ")
-    ano = input("Digite o ano do livro: ")
+    titulo = input("Digite o título do livro que deseja buscar: ").strip()
+    titulo_formato = urllib.parse.quote(titulo)
+    
+    url_da_api = f"https://www.googleapis.com/books/v1/volumes?q={titulo_formato}"
+
+    try:
+
+        print("Buscando online...")
+        response = requests.get(url_da_api)
+
+        #verifica se a resposta foi um sucesso ou um erro, se for erro vai levantar uma exceção http
+        response.raise_for_status()
+        dados = response.json()
+
+        if 'items' in dados and dados['items']:
+
+            livros_encontrados_api = dados['items']
+            print("\n--- Livros Encontrados ---")
+            print("Por favor, escolha um da lista abaixo:")
+
+            for i, livro_api in enumerate(livros_encontrados_api[:5], start=1):
+
+                info = livro_api.get('volumeInfo', {})
+
+                titulo_livro = info.get('title', 'Título não disponível')
+                lista_de_autores = info.get('authors', ['Autor desconhecido'])
+                primeiro_autor = lista_de_autores[0]
+                data_completa = info.get('publishedDate', '0000')
+                ano = data_completa[:4]
+
+                print(f"{i}: {titulo_livro}({ano}) por {primeiro_autor}")
+            
+            try:
+                escolha = 1 if len(livros_encontrados_api) == 1 else int(input(f"\nDigite o número do livro que deseja adicionar (1-{len(livros_encontrados_api[:5])}): " ))
+
+                if not(1 <= escolha <= len(livros_encontrados_api[:5])):
+                    print("Escolha inválida. Fora do intervalo de opções.")
+                    return
+
+                livro_escolhido_dict = livros_encontrados_api[escolha - 1]
+                
+
+                info = livro_escolhido_dict.get('volumeInfo', {})
+                titulo = info.get('title', 'Título Desconhecido')
+                autores = info.get('authors', ['Autor Desconhecido'])
+                autor = autores[0]
+                data_publicacao = info.get('publishedDate', '0000')
+                ano = data_publicacao[:4]
+
+                print(f"Você escolheu: {titulo} de {autor}")
+
+                novo_livro = Livro(titulo=titulo, autor=autor, ano_publicado=ano)
+
+                confirmar = input(f"Tem certeza que deseja adcionar? (s/n): ").lower().strip()
+
+                if confirmar == "s":
+                    if biblioteca_obj.adicionar_livro(novo_livro):
+                        print("Livro adicionado com Sucesso!")
+                    else:
+                        print("Erro ao adicionar livro. Verifique os dados e tente novamente.")
+                else:
+                    print("O livro não foi adicionado")
+
+            except ValueError:
+                print("Entrada inválida. Por favor, digite um número.")
+        else:
+            print(f"Nenhum livro encontrado com o título '{titulo}'.")
 
 
-    novo_livro = Livro(titulo, autor, ano)
+    #pega erros de conexão, timeout, URL inválida, e também os erros do raise_for_status
+    except requests.exceptions.RequestException as error:
+        print(f"Erro de conexão ou com a requisição: {error}")
+    
+    #pega o erro que acontece se o response.json falhar
+    except json.JSONDecodeError:
+        print("Erro: A resposta do servidor não é um JSON válido.")
+        return
 
-    if biblioteca_obj.adicionar_livro(novo_livro):
-        print("Livro adicionado com Sucesso!")
-    else:
-        print("Erro ao adicionar livro. Verifique os dados e tente novamente.")
+
 
 def gerenciar_biblioteca(biblioteca_obj):
 
@@ -94,6 +197,8 @@ def main():
             gerenciar_biblioteca(biblioteca)
 
         elif escolha == '0':
+            print("Salvando biblioteca antes de sair...")
+            biblioteca.salvar_em_json()
             print("Saindo...")
             break
 
