42855893047ee7c84f0219b33f3aab4336d792e0
Vinicius Alexandre <viniciusalexandrerodrigues7@gmail.com>
2025-06-16 01:46:23 -0300

    Adicionada funcionalidades de status e deletação de livros

--- Gerenciador_Livros.py
+++ Gerenciador_Livros.py
@@ -3,6 +3,8 @@ import json
 import requests
 import urllib.parse
 
+
+#Criação das Classes utilizadas
 class Livro:
 
     def __init__(self, titulo, autor, ano_publicado, status="quero ler"):
@@ -24,6 +26,7 @@ class Biblioteca:
         self.nome_arquivo = nome_arquivo
         self.carregar_de_json()
 
+    #Metodo para adicionar um livro na biblioteca
     def adicionar_livro(self, livro_obj):
 
         if any(not campo.strip() for campo in [livro_obj.titulo, livro_obj.autor, livro_obj.ano_publicado]):
@@ -33,10 +36,21 @@ class Biblioteca:
         self.livros.append(livro_obj)
         return True
 
+    #Metodo para retornar a listas de livros da biblioteca
     def listar_livros(self):
         return self.livros
 
 
+    #Metodo para apagar um livro escolhido da biblioteca
+    def apagar_livro(self, livro_deletar):
+
+        if livro_deletar in self.livros:
+            self.livros.remove(livro_deletar)
+            return True
+        return False
+
+
+    #Metodo para salvar a lista de livros em um arquivo json
     def salvar_em_json(self):
 
         salvamento_dos_dados = [{ 
@@ -54,7 +68,7 @@ class Biblioteca:
 
         print(f"Biblioteca salva em {self.nome_arquivo} com sucesso")
 
-
+    #Metodo para carregar um json e transformar em uma lista de livros na biblioteca
     def carregar_de_json(self):
 
         if os.path.exists(self.nome_arquivo):
@@ -167,10 +181,109 @@ def gerenciar_biblioteca(biblioteca_obj):
 
     if not livros:
         print("Sua biblioteca está vazia.")
-        return
+        return None
         
-    for livro in livros:
-        print(f"- {livro}")
+    for i, livro in enumerate(livros, start=1):
+        print(f"{i}- {livro}")
+    
+    return livros
+
+
+
+def apagar_um_livro(biblioteca_obj):
+
+    livros_encontrados = gerenciar_biblioteca(biblioteca_obj)
+
+    if livros_encontrados is None:
+        print("Não há livros adicionado na lista!")
+
+    try:
+        escolha = 1 if len(livros_encontrados) == 1 else int(input(f"Escolha o índice do livro que deseja excluir(1-{len(livros_encontrados)}):"))
+
+        if not (1 <= escolha <= len(livros_encontrados)):
+            print("Indice fora do intervalo")
+            return
+
+
+        livro_a_excluir = livros_encontrados[escolha-1]
+
+        confirmar = input(f"Tem certeza que deseja excluir o livro '{livro_a_excluir.titulo}' da sua biblioteca? (s/n): ").strip().lower()
+
+        if confirmar == "s":
+            if biblioteca_obj.apagar_livro(livro_a_excluir):
+                print("Livro excluido com sucesso!")
+            else:
+                print("Erro: O livro não foi encontrado na sua biblioteca para deleção.")
+        else:
+            print("Operação cancelada ")
+
+    except ValueError:
+        print("Entrada inválida. Por favor, digite um número.")
+    
+
+def mudar_status(biblioteca_obj):
+    
+    livros_encontrados = gerenciar_biblioteca(biblioteca_obj)
+
+    if livros_encontrados is None:
+        print("Não há livros adicionado na lista!")
+
+    try:
+        escolha = 1 if len(livros_encontrados) == 1 else int(input(f"Escolha o índice do livro que deseja mudar o status(1-{len(livros_encontrados)}):"))
+
+        if not (1 <= escolha <= len(livros_encontrados)):
+            print("Indice fora do intervalo")
+            return
+
+        livro_selecionado = livros_encontrados[escolha-1]
+
+        print(f"Status atual do livro ({livro_selecionado.titulo} - Status: {livro_selecionado.status})")
+
+        status_map = {
+                1: "quero ler",
+                2: "lendo",
+                3: "lido"}
+
+        escolha_status = int(input("Selecione o novo status do livro\n1- Quero ler\n2- Lendo\n3- Lido\nEscolha: "))
+
+
+        if escolha_status in status_map:
+            novo_status = status_map[escolha_status]
+            livro_selecionado.status = novo_status
+            print(f"Status do livro ({livro_selecionado.titulo}) definido como: {novo_status}")
+
+        else:
+            print("Escolha um índice válido")
+            return
+
+    except ValueError:
+        print("Entrada inválida. Por favor, digite um número.")
+    
+
+def mostrar_status(biblioteca_obj):
+
+    if not biblioteca_obj.livros:
+        print("Sua biblioteca está vazia.")
+        return
+    
+    livros_por_status = {}
+
+    for livro in biblioteca_obj.livros:
+        status_do_livro = livro.status
+
+        if status_do_livro not in livros_por_status:
+            livros_por_status[status_do_livro] = []
+        
+        livros_por_status[status_do_livro].append(livro)
+
+    print(f"Você possui um total de {len(biblioteca_obj.livros)} livros na sua biblioteca")
+    print("\nQuantidade atual de status de cada livro:")
+  
+    for status, lista_de_livros in livros_por_status.items():
+        print(f"\nLivros com status ({status}): {len(lista_de_livros)}")
+        for i, livro in enumerate(lista_de_livros, start=1):
+            print(f"{i} - {livro.titulo}")
+
 
 
     #================
@@ -186,17 +299,38 @@ def main():
         print("\n==== MENU DA BIBLIOTECA ====")
         print("1. Adicionar Livro")
         print("2. Listar Livros")
+        print("3. Apagar Livros")
+        print("4. Mostrar status dos livros")
+        print("5. Mudar status de leitura")
+        
+        
+
         print("0. Sair")
 
         escolha = input("Escolha uma opção: ").strip()
 
         if escolha == '1':
+            os.system("cls")
             gerenciar_adicao(biblioteca)
 
         elif escolha == '2':
+            os.system("cls")
             gerenciar_biblioteca(biblioteca)
 
+        elif escolha == '3':
+            os.system("cls")
+            apagar_um_livro(biblioteca)
+
+        elif escolha == '4':
+            os.system("cls")
+            mostrar_status(biblioteca)
+
+        elif escolha == '5':
+            os.system("cls")
+            mudar_status(biblioteca)
+
         elif escolha == '0':
+            os.system("cls")
             print("Salvando biblioteca antes de sair...")
             biblioteca.salvar_em_json()
             print("Saindo...")
